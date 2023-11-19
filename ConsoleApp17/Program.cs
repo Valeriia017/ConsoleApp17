6866f17a538bfdf7b583973a500600f6890e4a0e
Lerok <Lerok@DESKTOP-KFNQ071>
2023-11-19 18:50:13 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+// Клас події
+class Event
+{
+    public delegate void EventHandler();
+    public event EventHandler OnEvent;
+
+    public void GenerateEvent()
+    {
+        Console.WriteLine($"Сьогодні {DateTime.Now.Day}");
+        OnEvent?.Invoke();
+    }
+}
+
+// Клас Meter (Лічильник)
+class Meter
+{
+    public event EventHandler EventOccurred;
+
+    public void GenerateEvent()
+    {
+        Console.WriteLine($"Сьогодні {DateTime.Now.Day}. Час платити");
+        EventOccurred.Invoke();
+    }
+}
+
+// Клас Lodger (квартирознімач)
+class Lodger
+{
+    public string Name { get; set; }
+    public double AccountBalance { get; set; }
+    public double Debt { get; set; }
+
+    public Lodger(string name, double accountBalance, double debt)
+    {
+        Name = name;
+        AccountBalance = accountBalance;
+        Debt = debt;
+    }
+
+    public void HandleEvent()
+    {
+        Console.WriteLine($"{Name}, заплатив – {Debt}");
+        AccountBalance -= Debt;
+        Console.WriteLine($"На рахунку було – {AccountBalance + Debt}");
+        Console.WriteLine($"На рахунку залишилося {AccountBalance}");
+    }
+}
+
+// Клас ManagingCompany (ЖЕК)
+class ManagingCompany
+{
+    private Lodger lodger;
+
+    public ManagingCompany(Lodger lodger)
+    {
+        this.lodger = lodger;
+    }
+
+    public void HandleEvent()
+    {
+        Console.WriteLine($"Ваш борг – {lodger.Debt}");
+    }
+}
+
+// Клас Program
+class Program
+{
+    static void Main()
+    {
+        Event myEvent = new Event();
+        Meter meter = new Meter();
+        Lodger lodger = new Lodger("Іванов", 900, 500);
+        ManagingCompany managingCompany = new ManagingCompany(lodger);
+
+        myEvent.OnEvent += meter.GenerateEvent;
+        myEvent.OnEvent += lodger.HandleEvent;
+        myEvent.OnEvent += managingCompany.HandleEvent;
+
+        for (int i = 1; i <= 12; i++)
+        {
+            myEvent.GenerateEvent();
+        }
+    }
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+// Клас події
+class EventArguments : EventArgs
+{
+    public int CurrentDay { get; }
+
+    public EventArguments(int currentDay)
+    {
+        CurrentDay = currentDay;
+    }
+}
+
+// Клас Лічильник
+class Meter
+{
+    public event EventHandler<EventArguments> Event;
+
+    // Виводить повідомлення
+    public void GenerateEvent(int currentDay)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine($"Сьогодні {currentDay}");
+
+        if (currentDay == 10)
+        {
+            Console.WriteLine("Час оплати");
+            Event?.Invoke(this, new EventArguments(currentDay));
+        }
+    }
+}
+
+// Клас квартирознімач
+class Lodger
+{
+    private string name;
+    private int accountBalance;
+    private int debt;
+
+    public Lodger(string name, int initialBalance)
+    {
+        this.name = name;
+        this.accountBalance = initialBalance;
+        this.debt = 500; // Припустимо, що борг завжди 500
+    }
+
+    public int AccountBalance => accountBalance;
+    public int Debt => debt;
+
+    public void HandleEvent(object sender, EventArguments e)
+    {
+        if (e.CurrentDay == 10)
+        {
+            // Виводить повідомлення
+            Console.WriteLine($"{name}, заплатив – {debt}");
+            Console.WriteLine($"На рахунку було – {accountBalance + debt}");
+            Console.WriteLine($"На рахунку залишилося {accountBalance}");
+        }
+    }
+}
+
+// Клас ЖЕК
+class ManagingCompany
+{
+    private Lodger lodger;
+
+    public ManagingCompany(Lodger lodger)
+    {
+        this.lodger = lodger;
+    }
+
+    public void HandleEvent(object sender, EventArguments e)
+    {
+        // Виводить повідомлення
+        if (e.CurrentDay == 10)
+        {
+            Console.WriteLine($"Ваш борг – {lodger.Debt}");
+        }
+    }
+}
+
+// Клас тестування
+class Program
+{
+    static void Main()
+    {
+        Meter meter = new Meter();
+        Lodger lodger = new Lodger("Іванов", 400);
+        ManagingCompany managingCompany = new ManagingCompany(lodger);
+
+        meter.Event += managingCompany.HandleEvent;
+        meter.Event += lodger.HandleEvent;
+
+        for (int i = 1; i <= 16; i++)
+        {
+            meter.GenerateEvent(i);
+        }
+    }
+}
